bc937b952f7d25383adfdb85463ea3dfc7c3551b
PalumboAndrea <andreaseiuno.ap@gmail.com>
2022-11-18 15:50:53 +0100

    update

---
+++ js/myscript.js
@@ -0,0 +1,9 @@
+console.log ("Ciao");
+
+let nomeUtente = prompt ("Inserie nome utente");
+
+let cognomeUtente = prompt ("Inserie cognome utente");
+
+let colorePreferito = prompt ("Inserie il colore preferito dell'utente");
+
+document.getElementById('users-info') .innerHTML = nomeUtente + '' + cognomeUtente + '' + colorePreferito + ' 22'
